6f55277c1701083e920e0bb1ee3ea0d0a9bf523a
vard0ni <ip_goose@mail.ru>
2021-10-20 17:57:17 +0700

    Added spdlog library

--- SimpleEngineEditor/src/main.cpp
+++ SimpleEngineEditor/src/main.cpp
@@ -7,7 +7,7 @@ class MyApp : public SimpleEngine::Application
 	// Logic for every game frame
 	virtual void on_update() override
 	{
-		std::cout << "Update frame: " << frame++ << std::endl;
+		//std::cout << "Update frame: " << frame++ << std::endl;
 	}
 
 	int frame = 0;
